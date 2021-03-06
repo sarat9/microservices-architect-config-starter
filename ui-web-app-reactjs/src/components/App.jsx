94c1e8d2bf4b9ff7ee55ed5f3cb749b8e5324cf6
Deekshith SN <deekshithsn@gmail.com>
2021-03-06 21:47:39 +0530

    Update App.jsx

--- ui-web-app-reactjs/src/components/App.jsx
+++ ui-web-app-reactjs/src/components/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   const [displayData, setDisplayData] = useState({url:''})
 
   const handleApiCall = async (e) => {
-    const url = 'http://35.238.203.251:9999/'+e.target.name;
+    const url = 'http://34.67.30.154:9999/'+e.target.name;
     displayData.url=url
     let options = {}
       try {
