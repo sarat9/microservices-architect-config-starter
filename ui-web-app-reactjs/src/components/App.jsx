49ae70615ef852c824849c0ef364c5211aa3aac8
f1suppa <112776537+f1suppa@users.noreply.github.com>
2022-11-10 08:09:34 -0500

    Update App.jsx

--- ui-web-app-reactjs/src/components/App.jsx
+++ ui-web-app-reactjs/src/components/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   const [displayData, setDisplayData] = useState({url:''})
 
   const handleApiCall = async (e) => {
-    const url = 'http://localhost:9999/'+e.target.name;
+    const url = 'http://3.23.100.100:9999/'+e.target.name;
     displayData.url=url
     let options = {}
       try {
